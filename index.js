523ce4cd372afb8f4708d8fa88160f40eeabb2ec
Elton De Santana <eltonanderson@yahoo.com.br>
2023-09-01 12:25:49 -0300

    adicionado servidor Render

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ObjectId } = require("mongodb");
 
 //admin
 //xygUJlVOzVYZ9zjd
+//endereco Servidor REnder: https://jornada-backend-oceanbrasil-jx2p.onrender.com
 
 // const url = "mongodb://localhost:27017";
 //const url = "mongodb://127.0.0.1:27017";
